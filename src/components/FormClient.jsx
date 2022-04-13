1874103ebdc165383a740251a7e8901125612560
dayanMichelle <dayaara6@gmail.com>
2022-04-13 17:23:38 -0400

    form style completed

--- src/components/FormClient.jsx
+++ src/components/FormClient.jsx
@@ -6,17 +6,72 @@ export const FormClient = () => {
             <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Add Client</h1>
             <Formik>
                 <Form>
-                    <div>
+                    <div className='mb-4'>
                         <label 
                         className='text-gray-800'
                         htmlFor='name'>
                             Name:
                         </label>
                         <Field 
+                        type='text'
                         id="name"
-                        placeholder="Enter your name" 
+                        placeholder="Enter customer name" 
                         className='mt-2 block w-full p-3 bg-gray-50' />
                     </div>
+                    <div className='mb-4'>
+                        <label 
+                        className='text-gray-800'
+                        htmlFor='company'>
+                            Company:
+                        </label>
+                        <Field 
+                        type='text'
+                        id="company"
+                        placeholder="Enter customer Company" 
+                        className='mt-2 block w-full p-3 bg-gray-50' />
+                    </div>
+                    <div className='mb-4'>
+                        <label 
+                        className='text-gray-800'
+                        htmlFor='email'>
+                            E-mail:
+                        </label>
+                        <Field 
+                        type='email'
+                        id="email"
+                        placeholder="Enter customer email" 
+                        className='mt-2 block w-full p-3 bg-gray-50' />
+                    </div>
+                    <div className='mb-4'>
+                        <label 
+                        className='text-gray-800'
+                        htmlFor='phone'>
+                            Phone:
+                        </label>
+                        <Field 
+                        type='tel'
+                        id="phone"
+                        placeholder="Enter customer phone" 
+                        className='mt-2 block w-full p-3 bg-gray-50' />
+                    </div>
+                    <div className='mb-4'>
+                        <label 
+                        className='text-gray-800'
+                        htmlFor='note'>
+                            Note:
+                        </label>
+                        <Field 
+                        as='textarea'
+                        type='text'
+                        id="note"
+                        placeholder="Notes..." 
+                        className='mt-2 block w-full p-3 bg-gray-50' />
+                    </div>
+                    <input 
+                    type='submit'
+                    value='Add Client'
+                    className='mt-5 w-full bg-orange-500 p-3 text-white uppercase font-bold text-lg'
+                    />
                 </Form>
             </Formik>
         </div>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -17,7 +17,7 @@ export const Layout = () => {
                 to="/clients/new">New Client</Link>
             </nav>
         </div>
-        <div className='md:w-3/4 p-10'>
+        <div className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
 
           <Outlet />
         </div>
