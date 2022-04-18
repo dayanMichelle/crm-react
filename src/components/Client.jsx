8726989c949275780bb34ce47f46cf996a3a28e1
dayanMichelle <dayaara6@gmail.com>
2022-04-18 12:56:55 -0400

    adding get clients

---
+++ src/components/Client.jsx
@@ -0,0 +1,38 @@
+
+const Client = ({client}) => {
+    const {name,company,email,phone,note} = client
+  return (
+    <tr className="border-b hover:bg-gray-50">
+        <td className="p-3">{name}</td>
+        <td className="p-3">
+            <p><span className="text-gray-800 uppercase font-bold">Email:</span>{email}</p>
+            <p><span className="text-gray-800 uppercase font-bold">Tel:</span>{phone}</p>
+        </td>
+        <td className="p-3">{company}</td>
+        <td className="p-3">
+        <button
+            className="bg-yellow-500 hover:bg-yellow-600 block w-full text-white p-2 uppercase font-bold text-xs "
+            type="button"
+            >
+                Ver
+            </button>
+            <button
+            className="bg-blue-600 hover:bg-blue-700 block w-full text-white p-2 uppercase font-bold text-xs mt-3"
+            type="button"
+            >
+                Editar
+            </button>
+            <button 
+            className="bg-red-600 hover:bg-red-700 block w-full text-white p-2 uppercase font-bold text-xs mt-3"
+            type="button"
+            >
+                Eliminar
+            </button>
+        </td>
+    </tr>
+        
+
+  )
+}
+
+export default Client
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { useEffect,useState } from "react"
+import Client from "../components/Client"
 
 export const Home = () => {
+  const [clients, setClients] = useState([])
+  useEffect(() => {
+    const getClientAPI =  async  () =>
+    {
+      try {
+        const url = 'http://localhost:4000/clients'
+        const response = await fetch(url);
+        const result = await response.json()
+        setClients(result)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getClientAPI()
+  }, [])
   return (
-    <div>Home</div>
+    <>
+    <h1 className="font-black text-4xl text-orange-600">Clients</h1>
+    <p className="mt-3">Manage yours client</p> 
+
+    <table className="w-full mt-5 table-auto shadow bg-white">
+      <thead className="bg-blue-800 text-white">
+      <tr>
+        <th className="p-2">Name</th>
+        <th className="p-2">Company</th>
+        <th className="p-2">Email</th>
+        <th className="p-2">Phone</th>
+        <th className="p-2">Notes</th>
+      </tr>
+      </thead>
+      <tbody>
+          {clients.map(client => (
+
+            <Client 
+            key={client.id}
+            client={client}
+            />
+          
+
+          ))}
+      </tbody>
+    </table>
+   </>
   )
 }
